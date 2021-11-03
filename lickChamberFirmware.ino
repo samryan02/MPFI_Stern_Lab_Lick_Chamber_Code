acf7531c3a6cb47d3acb8a298a4c41d69954bc3b
Samuel.Breslin@mpfi.org <BreslinS@mpfi.org>
2021-11-03 16:59:37 -0400

    added comments to firmware

--- lickChamberFirmware.ino
+++ lickChamberFirmware.ino
@@ -1,41 +1,49 @@
 /*
 Samuel Breslin
 MPFI Mechanical Workshop Core
-MPFI Fitzpatick Lab
+MPFI Stern Lab
 */
 
+
+// create variables for last lick timer (used to prevent double counting of one lick)
 volatile int channel_1_past = 0;
 volatile int channel_2_past = 0;
 volatile int channel_3_past = 0;
 volatile int channel_4_past = 0;
 
+
+// create a threshold for double licks
 int double_lick_threshold = 50;
 
 
-
+// initialize packet info for communication
 int packetNumber = 0;
 int values[5] = {packetNumber, 0, 0, 0, 0};
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
+
+  //create interupts
   attachInterrupt(digitalPinToInterrupt(2), channel_1, FALLING);
   attachInterrupt(digitalPinToInterrupt(3), channel_2, FALLING);
   attachInterrupt(digitalPinToInterrupt(18), channel_3, FALLING);
   attachInterrupt(digitalPinToInterrupt(19), channel_4, FALLING);
+  //run for 30 minutes
   while(millis()< 18000000){
-    
+    //increment packets
     packetNumber++;
     Serial.print(packetNumber);
     Serial.print(", ");
     
+    send all data in current packet
     for(int i = 1; i< 5; i++){
       
       Serial.print(values[i]);
       Serial.print(", ");
         
     }
-    
+    //send time data
     Serial.print(millis());
     Serial.println();
     delay(200);
@@ -45,7 +53,7 @@ void setup() {
 void loop() {
   
 }
-
+// interupt functions
 void channel_1(){
   if(millis() - channel_1_past > double_lick_threshold) { 
     values[1]++;
