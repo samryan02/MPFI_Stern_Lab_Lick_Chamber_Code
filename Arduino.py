acb3771b3e320c46dd45a45f016740cd71008907
Samuel.Breslin@mpfi.org <BreslinS@mpfi.org>
2022-01-05 16:32:19 -0500

    added function for arduino communication abstraction

---
+++ Arduino.py
@@ -0,0 +1,35 @@
+import serial
+import re
+
+class Arduino():
+    
+
+    def __init__(self, commport):
+        self.arduino = serial.Serial(commport)
+        self.values = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+    def sendStartCommand(self):
+        self.arduino.write(bytes(b'start'))
+    def getFirstByte(self):
+        Data1 = self.arduino.readline().strip()
+        #print(Data1)
+        text = Data1.decode('UTF-8')
+        if(text == 'start'):
+            return(True)
+    def getData(self):
+        newData1 = self.arduino.readline().strip()
+        #print(newData1)
+        text = newData1.decode('UTF-8')
+        #print(text)
+
+        if(text == 'end'):
+            return([0,0,0,0,0,0,0,0,0,0,0,0])
+        formatedData1 = re.split(',',text)
+        #print(formatedData1)
+        for i in range(len(formatedData1)):
+            #print(formatedData1[i].strip())
+            self.values[i] = float(formatedData1[i].strip())
+        
+        self.values[11] = int(formatedData1[11].strip())/1000.0
+        return(self.values)
+
